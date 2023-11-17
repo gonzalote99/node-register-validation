b110926e4e63b265c3943b5cd92848145d50cd88
CodeSandbox <support@codesandbox.io>
2023-11-17 17:43:15 +0000

    Initial commit

--- index.js
+++ index.js
@@ -1,9 +1,55 @@
-var http = require("http");
+const express = require('express');
+const expresApp = express();
+const bodyParser = require('body-parser');
+const LocalStorage = require('node-localstorage').LocalStorage;
+const localStorage = new LocalStorage('./scratch');
 
-//create a server object:
-http
-  .createServer(function (req, res) {
-    res.write("Hello from CodeSandbox!"); //write a response to the client
-    res.end(); //end the response
-  })
-  .listen(8080); //the server object listens on port 8080
+expresApp.set('view engine', 'pug');
+expresApp.set('views', 'views');
+
+expresApp.use(bodyParser.urlencoded({extended: false}));
+
+expresApp.get('/', (req, res) => {
+  res.render('index')
+});
+
+expresApp.get('/sign-in', (req, res) => {
+  res.render('sign-in')
+});
+
+expresApp.get('/sign-up', (req, res) => {
+  res.render('sign-up')
+});
+
+expresApp.post('/sign-in', (req, res) => {
+  let person = JSON.parse(localStorage.getItem('person'));
+
+  if(person.name === req.body.name & person.password === req.body.password) {
+    res.redirect('/');
+  } else {
+    res.render('error')
+  }
+});
+
+expresApp.post('/sign-up', (req, res) => {
+  if(req.body.name !== '' & req.body.surname !== '' & req.body.email !== '' & req.body.password !== '' ) {
+    const person = {
+      name: req.body.name,
+      surname: req.body.surname,
+      email: req.body.email,
+      password: req.body.password,
+    }
+    let json = JSON.stringify(person);
+    localStorage.setItem('person', json)
+    res.redirect('/');
+  } else {
+    res.render('error');
+  }
+});
+
+expresApp.listen(3000, () => {
+  console.log('port 3000');
+})
+
+
+
